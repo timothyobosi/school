0de447d19f64594a9cf08c87b2f91145679cc4c0
timothyobosi <timothyobosi8@gmail.com>
2025-10-27 09:24:20 +0300

    first commit

---
+++ school/src/types/auth.ts
@@ -0,0 +1,13 @@
+
+
+export interface LoginFormData{
+    email: string;
+    password: string;
+}
+
+export interface SignupFormData{
+    name: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+}
